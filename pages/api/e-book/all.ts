89508edb7ed6e2ba8bfa8d83159d47a7947d6bee
Semet <hamdanilombok@gmail.com>
2022-11-05 16:42:34 +0800

    fix e-book page

--- pages/api/e-book/all.ts
+++ pages/api/e-book/all.ts
@@ -2,13 +2,30 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../prisma/db";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+	const limit = 6;
+	const cursor = req.query.cursor ?? "";
+	const cursorObj = cursor === "" ? undefined : { id: cursor.toString() };
 	try {
 		const books = await prisma.book.findMany({
 			orderBy: {
 				createdAt: "desc",
 			},
+			include: {
+				category: {
+					select: {
+						id: true,
+						name: true,
+					},
+				},
+			},
+			skip: cursor !== "" ? 1 : 0,
+			cursor: cursorObj,
+			take: limit,
 		});
-		res.status(200).json(books);
+		res.status(200).json({
+			books,
+			nextId: books.length === limit ? books[limit - 1].id : undefined,
+		});
 	} catch (e) {
 		res.status(500).json({
 			message: "Unable to load books ...",
--- pages/e-book/index.tsx
+++ pages/e-book/index.tsx
@@ -1,23 +1,43 @@
-import { Box, Button, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, GridItem, Skeleton, Text } from "@chakra-ui/react";
 import { Book } from "@prisma/client";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { NextPage } from "next";
 import Head from "next/head";
-import React from "react";
+import React, { Fragment } from "react";
 import EbookCard from "../../components/ebook/EbookCard";
 import LayoutSecondary from "../../components/LayoutSecondary";
 
 const EbookPage: NextPage = () => {
 	const {
-		data: allBooks,
 		isLoading,
 		isError,
-	} = useQuery<Book[]>(["allBooks"], async () => {
-		const res = await fetch("/api/e-book/all");
-		const data = await res.json();
+		data,
+		error,
+		isFetchingNextPage,
+		fetchNextPage,
+		hasNextPage,
+	} = useInfiniteQuery<{
+		nextId: string;
+		books: Book[];
+	}>(
+		["allBooks"],
+		async ({ pageParam = "" }) => {
+			await new Promise((res) => setTimeout(res, 1000));
+			const res = await fetch("/api/e-book/all?cursor=" + pageParam);
+			const data = await res.json();
 
-		return data;
-	});
+			return data;
+		},
+		{
+			getNextPageParam: (lastPage: any) => lastPage.nextId ?? false,
+		}
+	);
+	//
+	const loadMore = () => {
+		if (hasNextPage) {
+			fetchNextPage();
+		}
+	};
 	return (
 		<LayoutSecondary title={"Free E-book"}>
 			<Head>
@@ -27,31 +47,40 @@ const EbookPage: NextPage = () => {
 				<Flex gap={4}>
 					<Box w={{ base: "full" }} experimental_spaceY={"4"}>
 						<Grid gridTemplateColumns={"repeat(3, 1fr)"} gap={4}>
-							{!isLoading ? (
-								<>
-									{allBooks?.length ? (
-										<>
-											{allBooks.map((book) => (
-												<GridItem w={"full"} key={book.id}>
-													<EbookCard />
-												</GridItem>
-											))}
-										</>
-									) : (
-										<Text>No books ...</Text>
-									)}
-								</>
-							) : (
-								<Text>Loading ...</Text>
+							{isLoading && (
+								<Fragment>
+									{Array(3)
+										.fill(0)
+										.map((_, i) => (
+											<GridItem w={"full"} key={i}>
+												<Skeleton h={"500px"} />
+											</GridItem>
+										))}
+								</Fragment>
 							)}
+							{data &&
+								data.pages.map((page, index) => (
+									<Fragment key={index}>
+										{page.books.map((book) => (
+											<GridItem w={"full"} key={book.id}>
+												<EbookCard />
+											</GridItem>
+										))}
+									</Fragment>
+								))}
 						</Grid>
-						{allBooks !== undefined && allBooks.length > 9 ? (
-							<Box display={"flex"} justifyContent={"center"}>
-								<Button size={"lg"} colorScheme={"orange"} rounded={"3xl"}>
-									Load more
-								</Button>
-							</Box>
-						) : null}
+						<Box display={"flex"} justifyContent={"center"}>
+							<Button
+								size={"lg"}
+								colorScheme={"orange"}
+								rounded={"3xl"}
+								disabled={!hasNextPage}
+								isLoading={isFetchingNextPage}
+								onClick={loadMore}
+							>
+								{hasNextPage ? "Load more" : "No more"}
+							</Button>
+						</Box>
 					</Box>
 					<Box display={{ base: "none", lg: "flex" }} w={"30%"} bg={"green"}>
 						{/* Sidebar */}
